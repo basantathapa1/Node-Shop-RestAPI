97e07548a498a45ea99b0f5b466b42ff3fa542ed
Basanta Thapa <basantathapa@olivemedia.co>
2018-12-20 16:09:04 +0545

    Added basic http request and error handling

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan'); //morgan is for showing logs
+const bodyParser = require('body-parser');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// for managing CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allo-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Methods', 'PUT, POST, DELETE, PATCH, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+//Routes which should handle requests
+app.use('/products', productRoutes); //middleware
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error : {
+            message : error.message
+        }
+    });
+});
+
+module.exports = app;
